3411f91dbee52556676356aa52c441ef0537901e
nrob1029 <43589196+nrob1029@users.noreply.github.com>
2019-12-22 12:58:52 -0500

    Add project files.

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        public List<Person> TeamMemmbers { get; set; } = new List<Person>;
+
+        public string TeamName { get; set };
+
+    }
+}
